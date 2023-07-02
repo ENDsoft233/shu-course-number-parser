eaa0f25e3caf39940266c4c516e3b265657e09bb
Ray <i@r-ay.cn>
2023-07-02 22:34:18 +0800

    zap: transfer func getColleges to sync

--- src/funcs/list.ts
+++ src/funcs/list.ts
@@ -23,66 +23,58 @@ export function getLessonPropertiesList(): QueryListWithCode[] {
  * @param property `property.code` 生成正则表达式用, 课程属性, 默认匹配全部
  * @returns interface `{ name: string, regex: RegExp }[]`
  */
-export async function getCollegesList(property = '[a-zA-Z0-9]{2}'): Promise<QueryList[]> {
+export function getCollegesList(property = '[a-zA-Z0-9]{2}'): QueryList[] {
   const list: QueryList[] = [];
   interface BucketOfColleges {
     [key: string]: { codes: string[]; majors: string[] };
   }
   const collegesBucket: BucketOfColleges = {};
-  await Promise.all(
-    Object.entries(colleges).map(async ([collegeCode, college]) => {
-      if (college instanceof Array) {
-        for (const col of college) {
-          const majorCodes: string[] = [];
-          await Promise.all(
-            Object.entries(col).map(([majorCode]) => {
-              if (majorCode !== 'self') majorCodes.push(majorCode);
-            }),
-          );
-          if (typeof collegesBucket[col.self] === 'undefined') {
-            collegesBucket[col.self] = {
-              codes: [collegeCode],
-              majors: majorCodes,
-            };
-          } else {
-            collegesBucket[col.self].codes.push(collegeCode);
-            collegesBucket[col.self].majors.push(...majorCodes);
-          }
-        }
-      } else {
+  Object.entries(colleges).forEach(([collegeCode, college]) => {
+    if (college instanceof Array) {
+      for (const col of college) {
         const majorCodes: string[] = [];
-        await Promise.all(
-          Object.entries(college).map(([majorCode]) => {
-            if (majorCode !== 'self') majorCodes.push(majorCode);
-          }),
-        );
-        if (typeof collegesBucket[college.self] === 'undefined') {
-          collegesBucket[college.self] = {
+        Object.entries(col).map(([majorCode]) => {
+          if (majorCode !== 'self') majorCodes.push(majorCode);
+        });
+        if (typeof collegesBucket[col.self] === 'undefined') {
+          collegesBucket[col.self] = {
             codes: [collegeCode],
             majors: majorCodes,
           };
         } else {
-          collegesBucket[college.self].codes.push(collegeCode);
-          collegesBucket[college.self].majors.push(...majorCodes);
+          collegesBucket[col.self].codes.push(collegeCode);
+          collegesBucket[col.self].majors.push(...majorCodes);
         }
       }
-    }),
-  );
-  await Promise.all(
-    Object.entries(collegesBucket).map(async ([collegeName, college]) => {
-      list.push({
-        name: collegeName,
-        regex: new RegExp(
-          `^${college.codes.length === 1 ? college.codes[0] : `(${college.codes.join('|')})`}${
-            college.majors.length === 0
-              ? '([a-zA-Z0-9]{2})'
-              : college.majors.length === 1
-              ? college.majors[0]
-              : `(${college.majors.join('|')})`
-          }${property.length === 1 ? `(${property})([0-9]{1})` : `(${property})`}([0-9]{2})$`,
-        ),
+    } else {
+      const majorCodes: string[] = [];
+      Object.entries(college).forEach(([majorCode]) => {
+        if (majorCode !== 'self') majorCodes.push(majorCode);
       });
-    }),
-  );
+      if (typeof collegesBucket[college.self] === 'undefined') {
+        collegesBucket[college.self] = {
+          codes: [collegeCode],
+          majors: majorCodes,
+        };
+      } else {
+        collegesBucket[college.self].codes.push(collegeCode);
+        collegesBucket[college.self].majors.push(...majorCodes);
+      }
+    }
+  });
+  Object.entries(collegesBucket).forEach(([collegeName, college]) => {
+    list.push({
+      name: collegeName,
+      regex: new RegExp(
+        `^${college.codes.length === 1 ? college.codes[0] : `(${college.codes.join('|')})`}${
+          college.majors.length === 0
+            ? '([a-zA-Z0-9]{2})'
+            : college.majors.length === 1
+            ? college.majors[0]
+            : `(${college.majors.join('|')})`
+        }${property.length === 1 ? `(${property})([0-9]{1})` : `(${property})`}([0-9]{2})$`,
+      ),
+    });
+  });
   return list;
 }
